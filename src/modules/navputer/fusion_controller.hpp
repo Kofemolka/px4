dc679d1914cd4ff190ace4930d863cc7d06d16ac
kofemolka <kubovich@gmail.com>
2026-08-06 21:43:54 +0300

    AGP aid latch

--- src/modules/navputer/fusion_controller.hpp
+++ src/modules/navputer/fusion_controller.hpp
@@ -3,6 +3,7 @@
 
 #include "EKF/ekf.h"
 
+#include <drivers/drv_hrt.h>
 #include <px4_platform_common/module_params.h>
 
 class FusionController : public ModuleParams {
@@ -19,12 +20,15 @@ private:
 
 	parameters *_params;
 
+	hrt_abstime _agp_enabled_time{0};
+
 	DEFINE_PARAMETERS(
 		// per-source fusion enable
 		(ParamBool<px4::params::NPT_FUSE_BARO>) _param_npt_fuse_baro,
 		(ParamBool<px4::params::NPT_FUSE_MAG>) _param_npt_fuse_mag,
 		(ParamBool<px4::params::NPT_FUSE_RNGBC>) _param_npt_fuse_rngbc,
-		(ParamBool<px4::params::NPT_FUSE_AGP0>) _param_npt_fuse_agp0
+		(ParamBool<px4::params::NPT_FUSE_AGP0>) _param_npt_fuse_agp0,
+		(ParamFloat<px4::params::NPT_FC_AGP_LATCH>) _param_npt_fc_agp_latch
 	)
 };
 
